47eb07ee814f38d6fed311908b67f52e02169316
khoi1204 <127846687+khoi1204@users.noreply.github.com>
2023-05-23 20:00:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+//Task 1: Viết hàm tính tổng, hiệu, tích, thương của 2 số
+
+function four(a, b)
+{
+  return "Sum: " + (a + b) + " Sub: " + (a - b) + " Mul: " + (a * b) + " Div: " + (a / b)
+}
+var result1 = four(10, 5)
+console.log(result1)
+// document.write("<br>")
+//Task 2: Viết hàm tính chu vi và diện tích hình chữ nhật
+function rec(l, w)
+{
+  return "P = " + ((l + w) * 2) +", S = " + (l * w) 
+}
+var result2 = rec(10, 15)
+console.log(result2)
+
+
+//Task 3: Viết hàm tính tổng từ 1 đến n
+//Skip
+
